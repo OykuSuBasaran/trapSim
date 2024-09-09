f62430076b7e724b4122ca3fb8f4cca475a5f2df
Öykü Su Başaran <93545839+OykuSuBasaran@users.noreply.github.com>
2024-09-09 11:18:15 +0300

    Update trapSim.py

--- trapSim.py
+++ trapSim.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-import math
+import Plate
 
 pygame.init()
 
@@ -12,23 +12,6 @@ pygame.display.set_caption("Trap Atışı Simülasyonu")
 ORANGE = (237, 77, 22)
 GREEN = (11, 127, 26)
 
-# Plak sınıfı
-class Plate:
-    def __init__(self, x, y, angle):
-        self.x = x
-        self.y = y
-        self.angle = angle
-        self.speed = 5  # Hareket hızı
-
-    def move(self):
-        # Açıya göre x ve y hareket miktarları hesabı
-        radian_angle = math.radians(self.angle)
-        self.x += self.speed * math.cos(radian_angle) * (-1 if self.angle < 0 else 1) #x ekseninde hareket
-        self.y -= self.speed * abs(math.sin(radian_angle))#y ekseninde hareket
-
-
-    def draw(self, screen):
-        pygame.draw.rect(screen, ORANGE, (self.x, self.y, 20, 10))  # 20x10 boyutunda turuncu plaka
 
 # Oyun döngüsü
 clock = pygame.time.Clock()
@@ -47,7 +30,7 @@ while running:
                 angle = random.choice([10, 15, 20, 30, 40, 45])  # Rastgele bir açı seç
                 direction = random.choice([-1, 1])  # Sağa (-1) veya sola (1) yön belirle
                 angle *= direction  # Açının yönünü belirle
-                new_plate = Plate(WIDTH // 2, HEIGHT, angle)  # Alt orta noktadan başlat
+                new_plate = Plate.Plate(WIDTH // 2, HEIGHT, angle)  # Alt orta noktadan başlat
                 plates.append(new_plate)  # Yeni plağı listeye ekle
 
     # Plak hareketi ve çizimi
